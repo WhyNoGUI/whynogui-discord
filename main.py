24d05f457d0b91ae2b1f2be784d1fbd379d22163
Moritz-Friedemann <mofriedemann@web.de>
2021-03-20 23:07:26 +0100

    cleaned up code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Mapping
+import collections.abc
 from typing import Type, Tuple, Optional, List, Any
 
 import discord
@@ -28,47 +28,48 @@ with players.context() as player_ctx:
         }
 
         def __init__(self):
-            self.active_games: Mapping[str, games.AbstractGame] = {}
+            self.active_games: collections.Mapping[str, games.AbstractGame] = {}
 
-        @commands.command('show your coins')
+        @commands.command(help='show your coins')
         async def coins(self, ctx: commands.Context):
             author: discord.User = ctx.author
-            # if author := ctx.author is not discord.User:
-            # return
-            await ctx.send(f"```@{author.display_name}, your current balance is {player_ctx.coins(str(author.id))}```")
 
-        @commands.command('how your rank')
+            embed = discord.Embed(title = author.display_name)
+            embed.add_field(name="Balance:", value=str(player_ctx.coins(str(author.id))))
+            await ctx.send(embed=embed)
+
+        @commands.command(help='how your rank')
         async def rank(self, ctx: commands.Context):
             author: discord.User = ctx.author
-            # if author := ctx.author is not discord.User:
-            # return
-            await ctx.send(f"```@{author.display_name}, your current rank is {player_ctx.rank(str(author.id))}```")
+
+            embed = discord.Embed(title=author.display_name)
+            embed.add_field(name="Rank:", value=str(player_ctx.rank(str(author.id))))
+            await ctx.send(embed=embed)
 
         @commands.command(help='show ranks and their costs')
         async def ranks(self, ctx: commands.Context):
             author: discord.Member
-            # if author := ctx.author is not discord.Member:
-            # return
-            await ctx.send(f"""```Beginner              0
-                                Jackass Penguin         200
-                                Little Penguin          500
-                                Chinstrap Penguin       1000
-                                Rockhopper Penguin      5000
-                                Yellow-eyed Penguin     10000
-                                Gentoo Penguin          20000
-                                Snares-crested Penguin  50000
-                                ERECT-crested Penguin   100000
-                                Adelie Penguin          200000
-                                Royal Penguin           300000
-                                King Penguin            400000
-                                Emperor Penguin         500000
-                                Addicted Penguin        1000000```""")
+            description = """```
+                             Beginner                      0
+                             Jackass Penguin             200
+                             Little Penguin              500
+                             Chinstrap Penguin          1000
+                             Rockhopper Penguin         5000
+                             Yellow-eyed Penguin       10000
+                             Gentoo Penguin            20000
+                             Snares-crested Penguin    50000
+                             ERECT-crested Penguin    100000
+                             Adelie Penguin           200000
+                             Royal Penguin            300000
+                             King Penguin             400000
+                             Emperor Penguin          500000
+                             Addicted Penguin        1000000```"""
+            embed = discord.Embed(title="Ranks:", description=description)
+            await ctx.send(embed=embed)
 
         @commands.command(help='spend your coins to reach the next rank')
         async def rankup(self, ctx: commands.Context):
-            author: discord.Member
-            if author := ctx.author is not discord.Member:
-                return
+            author: discord.Member = ctx.author
             if player_ctx.rank(str(author.id)) == "Addicted Penguin":
                 await ctx.send("```Already reached the max rank!```")
                 return
@@ -79,11 +80,12 @@ with players.context() as player_ctx:
 
 
     class Casino(commands.Cog):
-        _GAMES: Mapping[str, Type[games.AbstractGame]]
+        _GAMES: collections.Mapping[str, Type[games.AbstractGame]]
         _RPS = ["r", "p", "s"]
 
         def __init__(self):
             self._active_games = []
+            self._game_offers = []
 
         def _get_game(self, player: discord.User) -> Optional[List[Any]]:
             return discord.utils.find(lambda g: player in g, games)
@@ -98,48 +100,78 @@ with players.context() as player_ctx:
         async def new(self, ctx: commands.Context, amount: int):
             if not ctx.message.mentions:
                 await ctx.send(f"```You didn't mention an opponent!```")
+                return
             p1: discord.User = ctx.author
             p2: discord.User = ctx.message.mentions[random.randint(0, len(ctx.message.mentions) - 1)]
             p1coins: int = player_ctx.coins(str(p1.id))
             p2coins: int = player_ctx.coins(str(p2.id))
-            if p1coins < amount:
+            if p2 is None:
+                await ctx.send(f"```Sorry, failed to find opponent!```")
+            elif p1coins < amount:
                 await ctx.send(f"```You only have {p1coins} coins!```")
             elif p2coins < amount:
-                await ctx.send(f"```{p2.display_name}, only has {p2coins} coins!```")
-            elif p2 is None:
-                await ctx.send(f"```Sorry, failed to find opponent!```")
+                await ctx.send(f"```{p2.display_name} only has {p2coins} coins!```")
             elif self._get_game(p1) is not None:
-                await ctx.send("```Can not start a new game, you have to finish the current one first```")
+                await ctx.send("```You have to finish your current game before starting a new one!```")
             elif self._get_game(p2) is not None:
                 await ctx.send(f"```{p2.display_name} is already in a game!```")
             else:
-                await ctx.send(f"@{p2.name}, do you accept the game with {p1.display_name}? [bj!accept/bj!decline]")
-                await ctx.bot.wait_for('message', check=self._check(ctx.author), timeout=30)
-                self._active_games.append([p1, p2, amount])
-                await ctx.send(f"```Game between {p1.display_name} and {p2.display_name} started.\n"
-                               f"There are {amount} coins on the line```")
+                await ctx.send(f"{p2.mention()}, {p1.display_name} challenges you to a game of rock, paper, scissors\n"
+                               f"[bj!accept/bj!decline]")
+                # await ctx.bot.wait_for('message', check=self._check(ctx.author), timeout=30)
+                self._game_offers.append([p1, p2, amount])
+                # await ctx.send(f"```Game between {p1.display_name} and {p2.display_name} started.\n"
+                # f"There are {amount} coins on the line```")
+
+        @commands.command()
+        async def accept(self, ctx: commands.Context):
+            author: discord.User = ctx.author
+            if not self._get_game(author) is None:
+                await ctx.send("```You have to finish your current game before starting a new one.```")
+            else:
+                offered = discord.utils.find(lambda g: author in g, self._game_offers)
+                if offered is None:
+                    await ctx.send("```There is no offer for you to accept.```")
+                else:
+                    self._active_games.append(offered)
+                    self._game_offers.remove(offered)
+                    await ctx.send(f"```Game between {author.display_name} and {offered[0].name} started.\n"
+                                   f"There are {offered[2]} coins on the line!```")
+
+        @commands.command()
+        async def decline(self, ctx: commands.Context):
+            author: discord.User = ctx.author
+            offered = discord.utils.find(lambda g: author in g, self._game_offers)
+            if offered is None:
+                await ctx.send("```There is no offer for you to decline.```")
+            else:
+                self._game_offers.remove(offered)
+                await ctx.send(f"```{offered[0].mention()} {author.display_name} declined your offer.```")
 
         @commands.command(help='pick your move for rock paper scissors (r/p/s)')
         async def play(self, ctx: commands.Context, symbol: str):
             author: discord.User = ctx.author
             game = self._get_game(author)
+            if game is None:
+                await ctx.send("```You aren't currently in a a game!```")
+            else:
+                await ctx.message.delete()
             if len(game) == 4:
                 other = Casino._RPS.index(game[3])
                 current = Casino._RPS.index(symbol)
                 if (other + 1) % 3 == current:
                     player_ctx.addCoins(str(game[0].id), -game[2])
                     player_ctx.addCoins(str(author.id), game[2])
-                    await ctx.send(f"```{author} has won```")
+                    await ctx.send(f"```{author.display_name} has won {game[2]} coins!```")
                 elif (other - 1) % 3 == current:
                     player_ctx.addCoins(str(author.id), -game[2])
                     player_ctx.addCoins(str(game[0].id), game[2])
-                    await ctx.send(f"```{game[0]} has won {game[2]} coins!```")
+                    await ctx.send(f"```{game[0].display_name} has won {game[2]} coins!```")
                 else:
                     await ctx.send("```It's a draw!```")
                 self._active_games.remove(game)
             else:
                 game.append(symbol)
-                await ctx.message.delete()
                 await ctx.send(f"```{author.display_name} has made a move```")
 
 
